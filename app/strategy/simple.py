e8453a3393b0748e682d78af576a702050ba6880
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2019-04-14 19:47:25 +0200

    added fix for simple strategy

--- app/strategy/simple.py
+++ app/strategy/simple.py
@@ -3,7 +3,7 @@ class StrategySimple(object):
         pass
 
     @staticmethod
-    def set_new_bet(bet, bet_result, table_limit=150.0):
+    def get_new_bet(bet, bet_result, table_limit=150.0):
         return bet['size_current']
 
     @staticmethod
