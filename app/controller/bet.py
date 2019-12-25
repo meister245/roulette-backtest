ab4575ff59fb6b863c0e8c87515af7ee7d1a0ecc
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2019-12-26 00:02:34 +0100

    adjusted for new factory. optimization

--- app/controller/bet.py
+++ app/controller/bet.py
@@ -1,21 +1,21 @@
 from app.factory.bet import BetFactory
-from app.model.roulette import RouletteModel
+from app.controller.roulette import RouletteController
 
 
 class BetController(object):
-    roulette_mdl = RouletteModel()
+    bet_fctr = BetFactory()
+    roulette_ctrl = RouletteController()
 
     def __init__(self, numbers):
+        self.bets = []
         self.results = []
         self.bet_configs = []
         self.numbers = numbers
 
-        self.bet_fctr = BetFactory()
-
     def get_bets_total_size(self):
         total_size = 0
 
-        for x in self.bet_fctr.bets:
+        for x in self.bets:
             total_size += x.size_current if x.is_bet_active() else 0
 
         return total_size
@@ -33,7 +33,7 @@ class BetController(object):
             if self.get_bets_total_size() > balance:
                 break
 
-            for bet in self.bet_fctr.bets:
+            for bet in self.bets:
                 balance, result = bet.run_bet(number, spin_count, balance, **kwargs)
 
                 if len(result) != 0:
@@ -43,29 +43,30 @@ class BetController(object):
 
         return tuple(self.results)
 
-    def set_bet_config(self, config: str) -> None:
+    @classmethod
+    def parse_bet_config(cls, config: str) -> dict:
         elements = [x for x in config.split(',') if len(x) != 0]
 
         if len(elements) < 5:
-            raise ValueError('invalid bet config - {}'.format(config))
+            raise ValueError(f'invalid model config - {config}')
 
-        self.bet_fctr.validate_bet_strategy(elements[0])
+        cls.bet_fctr.validate_bet_strategy(elements[0])
 
         config = {
             'strategy': elements[0],
-            'pattern': self.roulette_mdl.get_bet_pattern(elements[1]),
+            'pattern': cls.roulette_ctrl.get_bet_pattern(elements[1]),
             'size': elements[2],
-            'types': [self.roulette_mdl.validate_bet_type(x) for x in elements[3].split(':')],
+            'types': [cls.roulette_ctrl.validate_bet_type(x) for x in elements[3].split(':')],
             'limit_lose': elements[4] if 4 <= len(elements) else 0,
             'limit_win': elements[5] if 5 <= len(elements) else 0
         }
 
-        self.bet_configs.append(config)
+        return config
 
     def set_bets(self, numbers):
         for config in self.bet_configs:
             name = config['strategy']
             config['size'] = config['size']
 
-            if self.roulette_mdl.is_pattern_match(config['pattern'], numbers):
-                self.bet_fctr.set_bet(name, **config)
+            if self.roulette_ctrl.is_pattern_match(config['pattern'], numbers):
+                self.bets.append(self.bet_fctr.get_bet(name, **config))
